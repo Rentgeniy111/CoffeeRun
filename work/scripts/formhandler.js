2b4a8c2eb3d4f8f3c61a94a6d99095f8a95db82d
Rinat Gimadeev <r.gimadeev@danaflex-digital.ru>
2023-04-04 15:18:39 +0300

    222

--- work/scripts/formhandler.js
+++ work/scripts/formhandler.js
@@ -1,5 +1,6 @@
 (function (window) {
   "use strict";
+
   var App = window.App || {};
   var $ = window.jQuery;
   function FormHandler() {
@@ -13,13 +14,20 @@
     }
   }
 
-  FormHandler.prototype.addSubmitHandler = function () {
+  FormHandler.prototype.addSubmitHandler = function (fn) {
     console.log("Setting submit handler for form");
     this.$formElement.on("submit", function (event) {
       event.preventDefault();
 
-      var data = $(this).serializeArray();
+      var data = {};
+      $(this)
+        .serializeArray()
+        .forEach(function (item) {
+          data[item.name] = item.value;
+          console.log(item.name + " is " + item.value);
+        });
       console.log(data);
+      fn(data);
     });
   };
 
--- work/scripts/main.js
+++ work/scripts/main.js
@@ -3,7 +3,10 @@
   let App = window.App;
   let Truck = App.Truck;
   let DataStore = App.DataStore;
+  let FORM_SELECTOR = '[data-coffee-order="form"]';
   let myTruck = new Truck("ncc-1701", new DataStore());
   window.myTruck = myTruck;
-  window.App = App;
+  let formHandler = new FormHandler(FORM_SELECTOR);
+  formHandler.addSubmitHandler();
+  console.log(formHandler);
 })(window);
