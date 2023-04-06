6cc448bf1d6016ae658eac3a6cd3743e7fb8751b
rentgeniy111 <rentgeniy111@gmail.com>
2023-04-06 11:05:51 +0300

    added checklist functionality

--- work/scripts/main.js
+++ work/scripts/main.js
@@ -1,10 +1,12 @@
 (function (window) {
   "use strict";
+  let FORM_SELECTOR = '[data-coffee-order="form"]';
+  let CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
   let App = window.App;
   let Truck = App.Truck;
   let DataStore = App.DataStore;
-  let FORM_SELECTOR = '[data-coffee-order="form"]';
   let FormHandler = App.FormHandler;
+  let CheckList = App.CheckList;
   let myTruck = new Truck("ncc-1701", new DataStore());
   window.myTruck = myTruck;
   let formHandler = new FormHandler(FORM_SELECTOR);
